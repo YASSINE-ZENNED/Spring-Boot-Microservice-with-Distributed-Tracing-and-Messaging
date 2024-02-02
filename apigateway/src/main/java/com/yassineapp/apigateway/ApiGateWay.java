bb356b2f8a3033b4d6d49167ef16fcb8930e4132
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-02-02 18:47:39 +0100

    adding api gateway / load balancer

---
+++ apigateway/src/main/java/com/yassineapp/apigateway/ApiGateWay.java
@@ -0,0 +1,14 @@
+package com.yassineapp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGateWay {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiGateWay.class, args);
+    }
+}
